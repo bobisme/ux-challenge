bc8f006875fb0141d557c1600aef718c3e195b99
BoB <shiftsource@gmail.com>
2016-06-11 20:48:44 -0400

    Modal login working.

---
+++ pub/js/login.js
@@ -0,0 +1,19 @@
+(function(){
+  var screen = document.getElementById('modalScreen');
+  var login = document.getElementById('login');
+  function closeModal(ev) {
+    screen.className = screen.className.replace(/ *\bmodal-screen--visible\b/, '');
+  }
+
+  function openModal() {
+    screen.className = screen.className + ' modal-screen--visible';
+
+  }
+
+  screen.addEventListener('click', function(ev){
+    if (ev.target !== screen) return true;
+    closeModal();
+  });
+  document.querySelector('.modal__closer').addEventListener('click', closeModal);
+  login.addEventListener('click', openModal);
+})();
